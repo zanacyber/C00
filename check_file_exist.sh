c6cb6b1505b80dc583aecace216b008fff79cfc5
zanacyber <vdah010@gmail.com>
2025-05-11 03:30:09 +0530

    ok11

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,26 +1,12 @@
 #!/bin/bash
 
-# Demande le nom du fichier
-echo -n "Entrez le nom du fichier : "
-read fichier
-
-# Vérifie si aucun nom n'a été entré
-if [ -z "$fichier" ]; then
-    echo "Erreur : aucun nom de fichier n'a été fourni."
-    exit 1
-fi
+# Demande à l'utilisateur un nom de fichier 
+echo "Entrez le nom du fichier : "
+fichier=$(cat)
 
 # Vérifie si le fichier existe
-if [ ! -e "$fichier" ]; then
+if [ -f $fichier ]; then
     echo "Le fichier '$fichier' existe."
-    exit 1
+else
+    echo "Le fichier '$fichier' n'existe pas."
 fi
-
-# Vérifie si c'est un fichier régulier
-if [ ! -f "$fichier" ]; then
-    echo "Le fichier '$fichier' existe."
-fi
-
-
-# Si tout est OK
-echo "Succès : le fichier '$fichier' existe."
